a8d2827bf172e721acf584e08950f399b85a9472
Gaurav Arora <g_arora@hotmail.com>
2016-08-14 17:11:36 +0530

    completed answer with complete example

--- Chapter-10/SOLID_Questions/Question5_answer/Program.cs
+++ Chapter-10/SOLID_Questions/Question5_answer/Program.cs
@@ -7,23 +7,74 @@ namespace Question5_answer
     {
         static void Main(string[] args)
         {
-            var notifications = (new List<INotify>
-            {
-                new OnPremiseNotifier(),
-                new CloudNotifier()
-            });
-            var notification = new Notification(notifications);
-            notification.SendAll();
+            //CientA
+            IClient clientA = new ClientA();
+            var subscriptionsForA = clientA.Subscriptions();
+            Console.WriteLine("ClientA subscribed for {0} providers.",subscriptionsForA.Item1);
+            var notifyToClientA = new Notification(subscriptionsForA.Item2);
+            notifyToClientA.SendAll();
+
+            //ClientB
+            IClient clientB = new ClientB();
+            var subscriptionsForB = clientB.Subscriptions();
+            Console.WriteLine("ClientB subscribed for {0} providers.", subscriptionsForB.Item1);
+            var notifyToClientB = new Notification(subscriptionsForB.Item2);
+            notifyToClientB.SendAll();
+
+            //ClientC
+            IClient clientC = new ClientC();
+            var subscriptionsForC = clientC.Subscriptions();
+            Console.WriteLine("ClientC subscribed for {0} providers.", subscriptionsForC.Item1);
+            var notifyToClientC = new Notification(subscriptionsForC.Item2);
+            notifyToClientC.SendAll();
 
             Console.ReadLine();
         }
     }
 
+    public interface IClient
+    {
+        //Used tuple just to keep it simple
+        //You can use, whatever you want (key,value) etc.
+        Tuple<int, IEnumerable<IProcess>> Subscriptions();
+    }
+
+    public class ClientA : IClient
+    {
+        public Tuple<int, IEnumerable<IProcess>> Subscriptions()
+        {
+            //Get list of subscribed providers or notifier whatever you called it
+            //Best bet to get this list is database
+            //ClientA - subscribed for only one provider
+            return new Tuple<int, IEnumerable<IProcess>>(1, new List<IProcess> {new OnPremiseProvider()});
+        }
+    }
+
+    public class ClientB : IClient
+    {
+        public Tuple<int, IEnumerable<IProcess>> Subscriptions()
+        {
+            //ClientB - subscribed for two providers
+            return new Tuple<int, IEnumerable<IProcess>>(2,
+                new List<IProcess> {new OnPremiseProvider(), new CloudNotifier()});
+        }
+    }
+
+    public class ClientC : IClient
+    {
+        public Tuple<int, IEnumerable<IProcess>> Subscriptions()
+        {
+            //ClientB - subscribed for three providers
+            return new Tuple<int, IEnumerable<IProcess>>(3,
+                new List<IProcess> {new OnPremiseProvider(), new CloudNotifier(), new GalaxyNotifier()});
+        }
+    }
+
     public class Notification
     {
-        private readonly List<INotify> _providerList;
+        private readonly IEnumerable<IProcess> _providerList;
 
-        public Notification(List<INotify> providerList)
+        public Notification(IEnumerable<IProcess> providerList)
         {
             _providerList = providerList;
         }
@@ -32,29 +83,46 @@ namespace Question5_answer
         {
             foreach (var notificationProvider in _providerList)
             {
-                notificationProvider.Notify();
+                notificationProvider.Process();
             }
         }
     }
 
-    public interface INotify
+    public interface IProcess
     {
-        void Notify();
+        void Process();
     }
 
-    public class OnPremiseNotifier : INotify
+    public class OnPremiseProvider : IProcess
     {
-        public void Notify()
+        public void Process()
         {
+            //This should be used more than just a message or notification
+            //Assuming there are few business rules for this particular provider
+            //proces those rules and change code accordingly
             Console.WriteLine("You're getting these notifications because you opted for OnPremise Notifications....");
         }
     }
 
-    public class CloudNotifier : INotify
+    public class CloudNotifier : IProcess
     {
-        public void Notify()
+        public void Process()
         {
+            //This should be used more than just a message or notification
+            //Assuming there are few business rules for this particular provider
+            //proces those rules and change code accordingly
             Console.WriteLine("You're getting these notifications because you opted for Cloud Notifications....");
+        }
+    }
+
+    public class GalaxyNotifier : IProcess
+    {
+        public void Process()
+        {
+            //This should be used more than just a message or notification
+            //Assuming there are few business rules for this particular provider
+            //proces those rules and change code accordingly
+            Console.WriteLine("You're getting these notifications because you opted for Galaxy Notifications....");
         }
     }
 }
